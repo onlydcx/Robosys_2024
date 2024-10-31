6fff0971cc902d96636ee6f0254eaa21d70a73fd
onlydcx <rin.hanaoka@icloud.com>
2024-10-31 02:53:46 +0000

    update and add LICENCE

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Rin Hanaoka
+# SPDX-License-Identifier: BSD-3-Clause:
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +8,9 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINEMO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
