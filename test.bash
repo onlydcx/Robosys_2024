1c7ae012dd73b009d4381a8e6dad8b5aaeac5eb9
onlydcx <rin.hanaoka@icloud.com>
2024-11-28 14:20:49 +0000

    update test.bash

--- test.bash
+++ test.bash
@@ -1,16 +1,18 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2024 Rin Hanaoka
+# SPDX-FileCopyrightText: 2024 Rin Hanaoka <s23c1110vk@s.chibakoudai.jp>
 # SPDX-License-Identifier: BSD-3-Clause:
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+        echo ${1}行目が違うよ
+        res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+# print_divisorsへのテスト
+out=$(./print_divisors | echo 12)
+["${out}" = "[1, 2, 3, 4, 6, 12]"] || ng "$LINENO" 
 
-[ "${res}" = 0 ] && echo OK
-exit $res
+[ "$res" = 0 ] && echo ok
+exit "$res"
+
