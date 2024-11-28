2278a36443430c79edafe8f90f283b1c4259a0c3
onlydcx <rin.hanaoka@icloud.com>
2024-11-28 15:23:11 +0000

    Testing all behaviors

--- test.bash
+++ test.bash
@@ -10,9 +10,22 @@ ng () {
 res=0
 
 # print_divisorsへのテスト
+
+# 12の約数表示
 out=$(./print_divisors 12)
-echo $out
 [ "${out}" = "[1, 2, 3, 4, 6, 12]" ] || ng "$LINENO" 
+
+# 2048の約数表示
+out=$(./print_divisors 2048)
+[ "${out}" = "[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]" ] || ng "$LINENO"
+
+# 負の場合のエラーチェック
+out=$(./print_divisors -1)
+[ "$?" = 1 ] || ng "$LINENO"
+
+# 2つ以上の数字が入力された時のエラーチェック
+out=$(./print_divisors 10 20)
+[ "$?" = 1 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo ok
 exit "$res"
